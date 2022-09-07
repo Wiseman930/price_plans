555c0669525b4d688af9a55cbd5157d88ae96fcc
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-09-07 16:36:55 +0200

    start

--- tables.sql
+++ tables.sql
@@ -6,3 +6,24 @@ CREATE TABLE IF NOT EXISTS public.pricing_plans
     id serial NOT NULL primary key
 
 );
+
+CREATE TABLE IF NOT EXISTS public.users
+(
+	user_names varchar(250) not null,
+    id serial NOT NULL primary key,
+    link_id int,
+    FOREIGN KEY (link_id) REFERENCES pricing_plans(id)
+
+);
+
+INSERT INTO public.pricing_plans(
+	plan_name, sms_price, call_price , id)
+	VALUES ('sms100', 1, 2, 1);
+
+INSERT INTO public.pricing_plans(
+	plan_name, sms_price, call_price , id)
+	VALUES ('sms200', 1, 4, 2);
+
+INSERT INTO public.pricing_plans(
+	plan_name, sms_price, call_price , id)
+	VALUES ('call weekly', 1, 3, 3);
